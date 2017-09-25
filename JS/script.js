a8ef2b5100f1482afb0afe5c075b2f26cf190534
Johnathon Sykes <hireme@mrjsykes.com>
2017-09-25 12:06:40 -0700

    added logic for appending wiki results to page

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,25 @@
-console.log("We out cheaaaaa")
+$(document).ready(() => {
 
-$(document).ready(()=>{
+  $('#search-form').on('submit', function() {
+    const search = $('.search').val()
+    var url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&list=search&rvprop=content&srsearch=${search}&origin=*`
 
-  fetch('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&list=search&rvprop=content&srsearch=Los+Angeles&origin=*')
-    .then(res=>res.json())
-    .then((data)=>console.log(data))
+    fetch(url).then(res => res.json()).then((data) => {
+      var results = data.query.search
+      console.log(results)
+      results.map((element, key) => {
+        $('.results').append(`
+            <a href="http://en.wikipedia.org/?curid=${element.pageid}">
+            <div class="result">
+              <h3 className="result-number">${key + 1}</h3>
+              <h1 class="result-title">${element.title}</h1>
+              <p class="result-snippet">${element.snippet}</p>
+            </div>
+            </a>
+          `)
+      })
+    }).catch((error) => console.log(error))
+    return false
+  })
+
 })
